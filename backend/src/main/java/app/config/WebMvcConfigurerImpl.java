4c43853649a2ec61ea1d712d3fb8f30a41f869cd
user <email>
2021-12-24 19:10:34 +0300

    Add CORS Settings

---
+++ backend/src/main/java/app/config/WebMvcConfigurerImpl.java
@@ -0,0 +1,14 @@
+package app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfigurerImpl implements WebMvcConfigurer
+{
+    public void addCorsMappings(CorsRegistry registry)
+    {
+        registry.addMapping("/api/**").allowedOrigins("*");
+    }
+}
