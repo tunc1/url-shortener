acaed60af50e936917998426af6c683e3160ef8f
user <email>
2021-12-24 19:11:00 +0300

    Update AddNewURL

--- frontend/src/page/AddNewURL.js
+++ frontend/src/page/AddNewURL.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Navbar from "../component/Navbar";
+import { addNewURL } from "../util/APICall";
 
 function AddNewURL()
 {
+    const [shortUrl,setShortUrl]=useState();
     const formSubmit=e=>
     {
         const url={url:e.target.elements.url.value}
-        console.log(url);
+        addNewURL(url).then(savedUrl=>
+        {
+            setShortUrl(window.location.host+"/url/"+savedUrl.id);
+        });
         e.preventDefault();
     };
     return (
@@ -16,6 +22,10 @@ function AddNewURL()
                 <input type="submit" className="btn btn-success" value="Add"/>
                 <input type="reset" className="btn btn-warning" value="Clear"/>
             </form>
+            {
+                shortUrl&&
+                <h3><a href={shortUrl}>{shortUrl}</a></h3>
+            }
         </div>
       );
 }
