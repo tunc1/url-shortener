ea8b72ea22b9ba91619a72a4ad785a40de71ccb9
user <email>
2021-12-24 18:49:48 +0300

    Add Navbar

---
+++ frontend/src/component/Navbar.js
@@ -0,0 +1,10 @@
+function Navbar()
+{
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/">URL Shortener</a>
+        </nav>
+    );
+}
+
+export default Navbar;
